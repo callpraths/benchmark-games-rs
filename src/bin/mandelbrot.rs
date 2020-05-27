394415fc1a67d9c0ce788b029135e7442ee148c8
Prathmesh Prabhu <callpraths@gmail.com>
2020-05-26 21:51:28 -0700

    Replace raw output buffer with Vec
    This CL removes all unsafe code except the SSE2 instruction usage.
    
    Performance impact: Slight regression over raw pointer arithmetic:
    
    pprabhu@pprabhu-desktop:~/personal/benchmark-games-rs$ make mandelbrot-seq-bench
    Running mandelbrot sequential impl benchmarks...
    hyperfine \
            'target/release/mandelbrot 16000' \
            'originals/target/mandelbrot.gcc-seq-run 16000'
    Benchmark #1: target/release/mandelbrot 16000
      Time (mean ± σ):      3.795 s ±  0.002 s    [User: 3.787 s, System: 0.007 s]
      Range (min … max):    3.792 s …  3.798 s    10 runs
    
    Benchmark #2: originals/target/mandelbrot.gcc-seq-run 16000
      Time (mean ± σ):      3.719 s ±  0.001 s    [User: 3.712 s, System: 0.007 s]
      Range (min … max):    3.716 s …  3.721 s    10 runs
    
    Summary
      'originals/target/mandelbrot.gcc-seq-run 16000' ran
        1.02 ± 0.00 times faster than 'target/release/mandelbrot 16000'

--- src/bin/mandelbrot.rs
+++ src/bin/mandelbrot.rs
@@ -11,10 +11,8 @@
 // Will outlast current iteration.
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 
-use std::alloc::{alloc, dealloc, Layout};
 use std::arch::x86_64::__m128d;
 use std::io::Write;
-use std::mem;
 
 #[cfg(target_feature = "sse2")]
 mod mm {
@@ -205,63 +203,48 @@ fn calc_init_r_chunk(x: f64, wid_ht: f64) -> [__m128d; 4] {
 }
 
 fn main() {
-    unsafe {
-        // get width/height from arguments
-        let mut wid_ht: usize = 16000;
-        if !std::env::args().len() > 0 {
-            wid_ht = std::env::args().nth(1).unwrap().parse().unwrap();
-        }
-        wid_ht = (wid_ht + 7) & !(7 as usize);
-
-        std::io::stdout()
-            .write_all(format!("P4\n{} {}\n", wid_ht, wid_ht).as_bytes())
-            .unwrap();
-
-        // The original C program implicitly depended on the fact that malloc()
-        // returns memory that is at least aligned to 8 bytes.
-        // https://stackoverflow.com/questions/8752546/how-does-malloc-understand-alignment
-        let pixels_length = wid_ht * (wid_ht >> 3);
-        let pixels_layout = Layout::from_size_align(pixels_length, 8).unwrap();
-        let raw_pixels = alloc(pixels_layout);
-        let pixels: *mut mem::MaybeUninit<u8> = mem::transmute(raw_pixels);
-
-        let i0 = {
-            let mut i0: Vec<f64> = Vec::with_capacity(wid_ht);
-            for y in 0..wid_ht {
-                i0.push((2.0 / wid_ht as f64) * y as f64 - 1.0)
-            }
-            i0
-        };
-
-        // process 8 pixels (one byte) at a time
-        let r0 = {
-            let mut r0: Vec<[__m128d; 4]> = Vec::with_capacity(wid_ht / 8);
-            for x in (0..wid_ht).step_by(8) {
-                r0.push(calc_init_r_chunk(x as f64, wid_ht as f64));
-            }
-            r0
-        };
-
-        // TODO: Parallelize. From the original program:
-        // #pragma omp parallel for schedule(guided)
-        for (y, i) in i0.iter().enumerate() {
-            let init_i = mm::set_pd(*i, *i);
-            let rowstart = y * wid_ht / 8;
-            // Casting u64 to u8 works out in this case, but is clearly
-            // yucky.
-            // https://doc.rust-lang.org/reference/expressions/operator-expr.html#semantics
-            for (x, r) in r0.iter().enumerate() {
-                (*pixels.add(rowstart + x))
-                    .as_mut_ptr()
-                    .write(mand8(&*r, init_i) as u8);
-            }
-        }
-
-        let pixels: *const u8 = mem::transmute(pixels);
-        std::io::stdout()
-            .write_all(std::slice::from_raw_parts(pixels, pixels_length))
-            .unwrap();
-
-        dealloc(raw_pixels, pixels_layout);
+    // get width/height from arguments
+    let mut wid_ht: usize = 16000;
+    if !std::env::args().len() > 0 {
+        wid_ht = std::env::args().nth(1).unwrap().parse().unwrap();
     }
+    wid_ht = (wid_ht + 7) & !(7 as usize);
+
+    std::io::stdout()
+        .write_all(format!("P4\n{} {}\n", wid_ht, wid_ht).as_bytes())
+        .unwrap();
+
+    let mut pixels: Vec<u8> = Vec::with_capacity(wid_ht * (wid_ht >> 3));
+
+    let i0 = {
+        let mut i0: Vec<f64> = Vec::with_capacity(wid_ht);
+        for y in 0..wid_ht {
+            i0.push((2.0 / wid_ht as f64) * y as f64 - 1.0)
+        }
+        i0
+    };
+
+    // process 8 pixels (one byte) at a time
+    let r0 = {
+        let mut r0: Vec<[__m128d; 4]> = Vec::with_capacity(wid_ht / 8);
+        for x in (0..wid_ht).step_by(8) {
+            r0.push(calc_init_r_chunk(x as f64, wid_ht as f64));
+        }
+        r0
+    };
+
+    // TODO: Parallelize. From the original program:
+    // #pragma omp parallel for schedule(guided)
+    for (y, i) in i0.iter().enumerate() {
+        let init_i = mm::set_pd(*i, *i);
+        let rowstart = y * wid_ht / 8;
+        // Casting u64 to u8 works out in this case, but is clearly
+        // yucky.
+        // https://doc.rust-lang.org/reference/expressions/operator-expr.html#semantics
+        for (x, r) in r0.iter().enumerate() {
+            pixels.push(mand8(&*r, init_i) as u8);
+        }
+    }
+
+    std::io::stdout().write_all(pixels.as_slice()).unwrap();
 }
